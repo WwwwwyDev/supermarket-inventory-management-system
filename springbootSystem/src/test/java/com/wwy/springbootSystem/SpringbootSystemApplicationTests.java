7c0b00e1c3376aa7847e4e14c76a052d677fdc79
吴文益 <773323518@qq.com>
2021-06-16 14:00:09 +0800

    init

---
+++ springbootSystem/src/test/java/com/wwy/springbootSystem/SpringbootSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwy.springbootSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
