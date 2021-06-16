6e982a683564ef6f2f17bd971cf9043fa4d609a2
吴文益 <773323518@qq.com>
2021-06-16 14:41:42 +0800

    init

--- vue-system-ui/src/main.js
+++ vue-system-ui/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import installElementPlus from './plugins/element'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(router).mount('#app')
