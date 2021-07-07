35dc1f20655f72313e2bc94507a86173e5136ce7
吴文益 <773323518@qq.com>
2021-07-07 14:10:52 +0800

    修复了redis配置问题

--- gin-system/pkg/gredis/setup.go
+++ gin-system/pkg/gredis/setup.go
@@ -6,6 +6,9 @@ var RedisConn *Cacher
 
 func Setup()  {
 	options := Options{
+		Addr:setting.RedisSetting.Host,
+		Password:setting.RedisSetting.Password,
+		MaxActive:setting.RedisSetting.MaxActive,
 		MaxIdle:setting.RedisSetting.MaxIdle,
 		IdleTimeout:setting.RedisSetting.IdleTimeout,
 	}
